eb40783cc6b9d1fc00cdf226285eb144240094e9
Fazzi <faaris.ansari@proton.me>
2024-12-14 10:23:43 +0000

    fix growing pains with syncing from hyprland-pc

--- .local/scripts/brightness.sh
+++ .local/scripts/brightness.sh
@@ -2,28 +2,29 @@
 
 # Function to send brightness change notification
 notification() {
-	# Get current brightness level using ddcutil
-	brightness=$(ddcutil getvcp x10 | grep -oP 'current value\s*=\s*\K\d+')
-	# Send notification about the brightness change
-	dunstify -a "brightness" -u low -r "9999" -t 2000 -h int:value:"$brightness" -i "notification-display-brightness" "Brightness" "${brightness}%"
+    # Get current brightness level
+		brightness="$(xbacklight -get)"
+    # Send notification about the brightness change
+    dunstify -a "brightness" -u low -r "9999" -t 2000 -h int:value:"$brightness" -i "notification-display-brightness" "Brightness" "${brightness}%"
 }
 
 # Main script logic
 case $1 in
-up)
-	# Increase brightness by 10
-	ddcutil setvcp 10 + 10 --skip-ddc-checks --noverify --sleep-multiplier 0.2 --bus 4
-	ddcutil setvcp 10 + 10 --skip-ddc-checks --noverify --sleep-multiplier 0.2 --bus 5
-	;;
-down)
-	# Decrease brightness by 10
-	ddcutil setvcp 10 - 10 --skip-ddc-checks --noverify --sleep-multiplier 0.2 --bus 4
-	ddcutil setvcp 10 - 10 --skip-ddc-checks --noverify --sleep-multiplier 0.2 --bus 5
-	;;
-*)
-	echo "invalid cmd"
-	exit 1
-	;;
+    help)
+        show_help
+        exit 0
+        ;;
+    up)
+        # Increase brightness by the specified increment value
+        xbacklight -inc "$2"
+        ;;
+    down)
+        # Decrease brightness by the specified increment value
+        xbacklight -dec "$2"
+        ;;
+    *)
+        exit 1
+        ;;
 esac
 
 # Send notification after adjusting brightness
