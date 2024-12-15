6188bf2f08837df3c8e7e91ef33f617d71c81ba7
Fazzi <faaris.ansari@proton.me>
2024-12-15 00:59:16 +0000

    update README.md

--- .local/scripts/mpd-notif.sh
+++ .local/scripts/mpd-notif.sh
@@ -1,27 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-music_dir="$HOME/Music"
-previewdir="${XDG_CONFIG_HOME:-$HOME/.config}/ncmpcpp/previews"
-filename="${music_dir}/$(mpc --format '%file%' current)"
-previewname="$previewdir/$(mpc --format '%album%' current | base64).png"
+# Define the music directory
+MUSICDIR="$HOME/Music"
 
-# Ensure preview directory exists
-mkdir -p "$previewdir"
+# Temp file for the cover art in /tmp
+COVERART=$(mktemp /tmp/cover_XXXXXX.jpg)
 
-# Check for cover image files
-cover_image_found=false
-for ext in jpg jpeg png; do
-    if [[ -f "$(dirname "$filename")/cover.$ext" ]]; then
-        cp "$(dirname "$filename")/cover.$ext" "$previewname"
-        cover_image_found=true
-        break
-    fi
-done
+# Get the current song details
+TITLE=$(mpc --format '%title%' current)
+ARTIST=$(mpc --format '%artist%' current)
 
-# Extract cover art from audio file only if no cover image is found
-if ! $cover_image_found; then
-    ffmpeg -y -i "$filename" -map 0:v -vf scale=128:128 "$previewname" -loglevel error
-fi
+# Get the full path to the current song
+SONG_PATH="$MUSICDIR/$(mpc current -f %file%)"
 
-# Notify with the cover art
-notify-send -r 27072 "Now Playing" "$(mpc --format '%title% \n%artist%' current)" -i "$previewname"
+# Extract the cover art from the playing file
+ffmpeg -i "$SONG_PATH" -y -an -vcodec copy "$COVERART" 2>/dev/null
+
+# Send notification with title, artist, and cover art
+notify-send -r 27072 "Now Playing" "$TITLE \n$ARTIST" -i "$COVERART"
+
+# Clean up the temp file after sending the notification
+rm -f "$COVERART"
