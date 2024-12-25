40c6f8a44003116f9572fccf1b61ba88b38c970c
Fazzi <faaris.ansari@proton.me>
2024-12-25 14:22:05 +0000

    make foot font size smaller, disable debug in makepkg again, make conf zsh function less repetitive, other minor changes

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,21 +1,27 @@
 #!/usr/bin/env zsh
 
 function conf() {
-  case $1 in
-    hypr)     $EDITOR "$XDG_CONFIG_HOME/hypr/hyprland.conf" ;;
-    hyprlock) $EDITOR "$XDG_CONFIG_HOME/hypr/hyprlock.conf" ;;
-    hypridle) $EDITOR "$XDG_CONFIG_HOME/hypr/hypridle.conf" ;;
-    hyprpaper) $EDITOR "$XDG_CONFIG_HOME/hypr/hyprpaper.conf" ;;
-    xdph) $EDITOR "$XDG_CONFIG_HOME/hypr/xdph.conf" ;;
-    dunst)    $EDITOR "$XDG_CONFIG_HOME/wallust/templates/dunstrc" ;;
-    bottom) $EDITOR "$XDG_CONFIG_HOME/bottom/bottom.toml" ;;
-    fastfetch) $EDITOR "$XDG_CONFIG_HOME/fastfetch/config.jsonc" ;;
-    fuzzel) $EDITOR "$XDG_CONFIG_HOME/fuzzel/fuzzel.ini" ;;
-    wleave) $EDITOR "$XDG_CONFIG_HOME/wleave/layout" ;;
-    makepkg) $EDITOR "$XDG_CONFIG_HOME/pacman/makepkg.conf" ;;
-    paru) $EDITOR "$XDG_CONFIG_HOME/paru/paru.conf" ;;
-    foot)    $EDITOR "$XDG_CONFIG_HOME/foot/foot.ini" ;;
-  esac
+  declare -A configs=(
+    [hypr]="$XDG_CONFIG_HOME/hypr/hyprland.conf"
+    [hyprlock]="$XDG_CONFIG_HOME/hypr/hyprlock.conf"
+    [hypridle]="$XDG_CONFIG_HOME/hypr/hypridle.conf"
+    [hyprpaper]="$XDG_CONFIG_HOME/hypr/hyprpaper.conf"
+    [xdph]="$XDG_CONFIG_HOME/hypr/xdph.conf"
+    [dunst]="$XDG_CONFIG_HOME/wallust/templates/dunstrc"
+    [bottom]="$XDG_CONFIG_HOME/bottom/bottom.toml"
+    [fastfetch]="$XDG_CONFIG_HOME/fastfetch/config.jsonc"
+    [fuzzel]="$XDG_CONFIG_HOME/fuzzel/fuzzel.ini"
+    [wleave]="$XDG_CONFIG_HOME/wleave/layout"
+    [makepkg]="$XDG_CONFIG_HOME/pacman/makepkg.conf"
+    [paru]="$XDG_CONFIG_HOME/paru/paru.conf"
+    [foot]="$XDG_CONFIG_HOME/foot/foot.ini"
+  )
+
+  if [[ -n ${configs[$1]} ]]; then
+    $EDITOR "${configs[$1]}"
+  else
+    echo "Configuration for '$1' not found."
+  fi
 }
 
 function paste() {
@@ -25,7 +31,7 @@ function paste() {
 	wl-copy $link
 }
 
-
+# See: https://codeberg.org/dnkl/foot/wiki#user-content-spawning-new-terminal-instances-in-the-current-working-directory
 function osc7-pwd() {
     emulate -L zsh # also sets localoptions for us
     setopt extendedglob
